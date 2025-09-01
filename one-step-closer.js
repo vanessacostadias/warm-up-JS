089f13aeb846f4e0566c6519b2ffef8ae6b92a8d
Vanessa Coimbra <vanessacostadias@gmail.com>
2025-09-01 18:44:12 +0100

    Create one-step-closer.js
    Exercício: One step closer

---
+++ one-step-closer.js
@@ -0,0 +1,18 @@
+/* EXERCISE DESCRIPTION: Take the following text and turn it into computer output using console.logs. 
+The numbers 3, 2 and 1 must be printed as numbers, not strings. 
+The line "Pause for dramatic effect" should be left as a single-line comment and not used as output.
+I have a special announcement to make! Are you ready? Start the countdown...
+3
+2
+1
+Pause for dramatic effect
+You are one step closer to becoming a JavaScript programmer! */
+
+// MY SOLUTION:
+
+console.log('I have a special announcement to make! Are you ready? Start the countdown...');
+console.log(3);
+console.log(2);
+console.log(1);
+//Pause for dramatic effect
+console.log('You are one step closer to becoming a JavaScript programmer!');
